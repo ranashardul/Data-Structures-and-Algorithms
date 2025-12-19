c75c26585bd78f50a51b9840c09d0497d5923e48
Shardul Rana <shardulrana59@gmail.com>
2025-12-19 12:13:39 +0530

    Time: 4 ms (20.96%) | Memory: 49.2 MB (14.38%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,25 +1,44 @@
+import java.util.*;
+
 class Solution {
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
-        boolean[] vis = new boolean[n];
-        int cnt = 0;
 
-        for (int i =0; i<n;i++){
-            if(!vis[i]){
-                dfs(isConnected, vis, i);
-                cnt++;
+        // Step 1: Build adjacency list
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (isConnected[i][j] == 1 && i != j) {
+                    adj.get(i).add(j);
+                    adj.get(j).add(i);
+                }
             }
         }
 
-        return cnt;
+        // Step 2: DFS to count provinces
+        boolean[] visited = new boolean[n];
+        int provinces = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                dfs(adj, visited, i);
+                provinces++;
+            }
+        }
+
+        return provinces;
     }
 
-    private void dfs(int[][] isConnected, boolean[] vis, int city){
-        vis[city]=true;
+    private void dfs(List<List<Integer>> adj, boolean[] visited, int city) {
+        visited[city] = true;
 
-        for (int j=0; j<isConnected.length; j++){
-            if(isConnected[city][j]==1 && !vis[j]){
-                dfs(isConnected, vis, j);
+        for (int neighbor : adj.get(city)) {
+            if (!visited[neighbor]) {
+                dfs(adj, visited, neighbor);
             }
         }
     }
