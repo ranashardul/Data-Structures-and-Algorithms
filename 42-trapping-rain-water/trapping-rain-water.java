9c97340b1d6fce5e4a9f46201511a35904b3f71e
Shardul Rana <shardulrana59@gmail.com>
2026-02-17 16:25:24 +0530

    Time: 0 ms (100.00%) | Memory: 47.8 MB (58.07%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int trap(int[] arr) {
+        int lmax=0,rmax=0,total = 0;
+        int l = 0;
+        int r = arr.length - 1;
+
+        while(l<r){
+            if(arr[l]<=arr[r]){
+                if(lmax>arr[l]){
+                    total+=lmax-arr[l];
+                } else {
+                    lmax=arr[l];
+                }
+                l++;
+            } else {
+                if(rmax>arr[r]){
+                    total += rmax-arr[r];
+                } else {
+                    rmax=arr[r];
+                }
+                r--;
+            }
+        }
+
+        return total;
+    }
+}
