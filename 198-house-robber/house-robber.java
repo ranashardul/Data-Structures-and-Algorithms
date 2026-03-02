214c0f881db318df5eeb537ebf3dc96b6d2b8b49
Shardul Rana <shardulrana59@gmail.com>
2026-03-02 15:32:29 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (17.18%) - LeetSync

---
+++ 198-house-robber/house-robber.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp,-1);
+
+        return findMax(nums, n-1, dp);
+    }
+
+    private int findMax(int[] nums, int n, int[] dp){
+        if(n==0) return nums[n];
+        if(n<0) return 0;
+        if(dp[n]!=-1) return dp[n];
+
+        int pick = nums[n] + findMax(nums, n-2, dp);
+        int notpick = 0 + findMax(nums, n-1, dp);
+
+        return dp[n] = Math.max(pick, notpick);
+    }
+}
